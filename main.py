0d712c834ce54a230edbcf8af812c0d9b72b5fae
fevinfelix <fevinfelix@gmail.com>
2023-03-18 17:28:42 -0400

    fixed aspect ratio for uploaded images and clears screen on new upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+#%%
 import tkinter # to get this to import in a virtual environment, do "brew install python-tk@3.10 to match your python version"
 import customtkinter
 from PIL import Image,ImageTk,ImageOps
 from tkinter import filedialog
 import math
-from matplotlib.pyplot import text
 
 import ultralytics
 from ultralytics import YOLO
@@ -11,7 +11,7 @@ from ultralytics import YOLO
 model = YOLO("best7.pt")
 
 def print_detected(imgPath) -> list:
-    results = model.predict(source=imgPath)  # predict on an image
+    results = model.predict(source=imgPath, save=True)  # predict on an image
     classes = {
         0 : "apples",
         1 : "bananas",
@@ -28,7 +28,7 @@ def print_detected(imgPath) -> list:
             detected.append(classes[number])
     print(detected)
 
-
+#%%
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -43,7 +43,7 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((0, 1, 2, 3), weight=1)
 
         self.textbox = customtkinter.CTkTextbox(master=self)
-        self.textbox.grid(row=0, column=0, columnspan=4, padx=20, pady=(20, 0), sticky="nesw")
+        self.textbox.grid(row=0, column=0, columnspan=4, padx=0, pady=(0, 0), sticky="nesw")
 
         self.upload_button = customtkinter.CTkButton(master=self, command=self.upload_and_display, text="Upload Image")
         self.upload_button.grid(row=1, column=0, padx=10, pady=10, sticky="we")
@@ -58,19 +58,29 @@ class App(customtkinter.CTk):
         self.textbox.insert("insert", self.combobox.get() + "\n")
     
     def upload_and_display(self):
+        self.textbox = customtkinter.CTkTextbox(master=self)
+        self.textbox.grid(row=0, column=0, columnspan=4, padx=20, pady=(20, 0), sticky="nesw")
         text = self.textbox
+
         your_Image=filedialog.askopenfilename(title = "Select your image",filetypes = [("Image Files","*.png"),("Image Files","*.jpg")])
         img_File=Image.open(your_Image)
 
+        # resizes image
         original_h = img_File.height
         original_w = img_File.width
         target_area = (text.winfo_height())* (text.winfo_width())
         new_w = math.sqrt((original_w / original_h) * target_area)
         new_h = target_area / new_w
 
-        your_image = customtkinter.CTkImage(light_image=img_File, size=(new_w / 1.5, new_h / 1.5))
+        if (new_h > new_w):
+            new_h /= 1.5
+            new_w /= 1.5
+        else:
+            new_h /= 1.1
+            new_w /= 1.1 
+
+        your_image = customtkinter.CTkImage(light_image=img_File, size=(new_w, new_h))
         label = customtkinter.CTkLabel(master=text, image=your_image, text='')
         label.grid(column=0, row=0, columnspan=4)
-
 app = App()
 app.mainloop()
